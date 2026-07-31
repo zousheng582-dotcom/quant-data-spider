02c54f09c2a93e241980bd1028412f571fa7cfef
zousheng582-dotcom <zousheng582@gmail.com>
2026-07-31 21:32:54 +0800

    Update daily_spider.py

--- daily_spider.py
+++ daily_spider.py
@@ -1,8 +1,9 @@
 import json
-stock_data = [
-    {"code":"000001","name":"平安银行","price":10.23,"change":0.02},
-    {"code":"300750","name":"宁德时代","price":182.50,"change":-0.86}
-]
+import akshare as ak
+# 爬取A股大盘数据
+stock_index_zh_hist_df = ak.stock_index_zh_hist(symbol="000001")
+# 转json存入文件
+stock_data = stock_index_zh_hist_df.to_dict("records")
 with open("data.json","w",encoding="utf-8") as f:
     json.dump(stock_data,f,ensure_ascii=False,indent=2)
-print("爬虫执行完成，行情数据已更新")
+print("A股指数数据更新完成")
