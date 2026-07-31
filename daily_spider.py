a2514434b0630a3a3333422728276ef0fed7065e
zousheng582-dotcom <zousheng582@gmail.com>
2026-07-31 22:08:11 +0800

    Update daily_spider.py

--- daily_spider.py
+++ daily_spider.py
@@ -1,9 +1,10 @@
 import json
 import akshare as ak
-# 爬取A股大盘数据
-stock_index_zh_hist_df = ak.stock_index_zh_hist(symbol="000001")
-# 转json存入文件
-stock_data = stock_index_zh_hist_df.to_dict("records")
+
+# 获取上证指数日线数据（新版有效接口）
+df = ak.stock_zh_index_daily(symbol="sh000001")
+# 转为json写入文件
+stock_data = df.to_dict("records")
 with open("data.json","w",encoding="utf-8") as f:
     json.dump(stock_data,f,ensure_ascii=False,indent=2)
 print("A股指数数据更新完成")
